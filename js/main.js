1030720c9acd7b72a5514db5c7e42e9e1c5b7ccf
Sergei Savin <sergeisavinweb@gmail.com>
2021-12-09 21:11:30 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(() => {
+  $(".popup__link").magnificPopup({
+    disableOn: 700,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  });
+
+  $(".gallery__slider").slick({
+    prevArrow: "<button type=\"button\" class=\"slick-btn slick-prev\"><img src=\"img/gallery/arrow_left.svg\" alt=\"\"></button>",
+    nextArrow: "<button type=\"button\" class=\"slick-btn slick-next\"><img src=\"img/gallery/arrow_right.svg\" alt=\"\"></button>",
+  });
+
+  $(".gallery__item-inner").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+  });
+
+  $(".menu__btn").on("click", () => {
+    $(".menu__list").toggleClass("menu__list--active");
+  });
+});
